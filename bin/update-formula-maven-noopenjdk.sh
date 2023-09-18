9a76ed3098011c98afa4b7acd69c9e53b783ea4a
Gabriel Fernández <60903812+gabrielfn-vcs@users.noreply.github.com>
2023-09-18 15:07:14 -0500

    Update maven-noopenjdk.rb using latest homebrew-core/master/Formula/maven.rb

--- bin/update-formula-maven-noopenjdk.sh
+++ bin/update-formula-maven-noopenjdk.sh
@@ -12,7 +12,7 @@ git pull > /dev/null
 # Grab the latest maven.rb from Homebrew and generate a new Formula/maven-noopenjdk.rb
 fname="Formula/maven-noopenjdk.rb"
 echo "Updating [${fname}]"
-curl -s https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/maven.rb | perl -pe 's/class Maven < Formula/class MavenNoopenjdk < Formula/g; s/depends_on \"openjdk\"//g' | perl -0pe 's/bottle do.*?end//gms' > ${fname}
+curl -s https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/m/maven.rb | perl -pe 's/class Maven < Formula/class MavenNoopenjdk < Formula/g; s/depends_on \"openjdk\"//g' | perl -0pe 's/bottle do.*?end//gms' > ${fname}
 
 # Commit the change and push it to the remote repo
 echo "Committing and pushing changes to remote repo"
