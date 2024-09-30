036cc2770c689e02b3b149477c534e264e1a53f2
Gabriel Fernández <60903812+gabrielfn-vcs@users.noreply.github.com>
2024-09-29 22:11:58 -0500

    Update maven-noopenjdk.rb using latest homebrew-core/master/Formula/maven.rb

--- Formula/maven-noopenjdk.rb
+++ Formula/maven-noopenjdk.rb
@@ -1,9 +1,9 @@
 class MavenNoopenjdk < Formula
   desc "Java-based project management"
   homepage "https://maven.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=maven/maven-3/3.9.4/binaries/apache-maven-3.9.4-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/maven/maven-3/3.9.4/binaries/apache-maven-3.9.4-bin.tar.gz"
-  sha256 "ff66b70c830a38d331d44f6c25a37b582471def9a161c93902bac7bea3098319"
+  url "https://www.apache.org/dyn/closer.lua?path=maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz"
+  sha256 "7a9cdf674fc1703d6382f5f330b3d110ea1b512b51f1652846d9e4e8a588d766"
   license "Apache-2.0"
 
   livecheck do
@@ -19,7 +19,7 @@ class MavenNoopenjdk < Formula
 
   def install
     # Remove windows files
-    rm_f Dir["bin/*.cmd"]
+    rm(Dir["bin/*.cmd"])
 
     # Fix the permissions on the global settings file.
     chmod 0644, "conf/settings.xml"
@@ -54,6 +54,7 @@ class MavenNoopenjdk < Formula
         </properties>
       </project>
     EOS
+
     (testpath/"src/main/java/org/homebrew/MavenTest.java").write <<~EOS
       package org.homebrew;
       public class MavenTest {
@@ -62,6 +63,7 @@ class MavenNoopenjdk < Formula
         }
       }
     EOS
-    system "#{bin}/mvn", "compile", "-Duser.home=#{testpath}"
+
+    system bin/"mvn", "compile", "-Duser.home=#{testpath}"
   end
 end
